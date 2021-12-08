25541cec11d8d695657a15540b99e71abbe34581
Justin Kemp <jkemp0790@gmail.com>
2021-12-07 23:32:53 -0600

    more changes to functions creating markdown, created a variable to hold all html until we render it to the page, still having errors

--- classes/generateMarkdown.js
+++ classes/generateMarkdown.js
@@ -2,45 +2,6 @@ const Engineer = require("./engineer");
 const Intern = require("./intern");
 const Manager = require("./manager");
 
-// TODO: Create a function to generate markdown for html page
-function generateMarkdown(data) {
-    console.log('In GenerateMarkdown:', data);
-    
-    for (var i = 0; i < data.length; i++) {
-        console.log('Employees: ', data[i]);
-        if (data[i] === Manager) {
-            managerRender();
-        }
-        else if (data[i] === Engineer) {
-            engineerRender();
-        }
-        else if (data[i] === Intern) {
-            internRender();
-        }
-    }
-    return `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Team Profile</title>
-    <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
-    </head>
-    <body class="min-100-vh flex-column">
-    <main class="flex-row justify-center align-center col-auto p-4 bg-light">
-        <div id="div" class="card card-container" style="width: 14rem;">
-            ${data.res}
-        </div>
-    </main>
-    </body>
-</html>
-    `
-};
-
 function managerRender(res) {
     return `
     <div class="card" style="width: 14rem;">
@@ -48,7 +9,7 @@ function managerRender(res) {
             <h3>${res.name}</h3>
             <h5>Manager</h5>
         </div>
-        <div class="card-body d-flex justify-content-center card-container">
+        <div class="card-body d-flex justify-content-center">
             <div class="card" style="width: 11rem;">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${res.id}</li>
@@ -68,7 +29,7 @@ function engineerRender(res) {
             <h3>${res.name}</h3>
             <h5>Engineer</h5>
         </div>
-        <div class="card-body d-flex justify-content-center card-container">
+        <div class="card-body d-flex justify-content-center">
             <div class="card" style="width: 11rem;">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${res.id}</li>
@@ -88,7 +49,7 @@ function internRender(res) {
             <h3>${res.name}</h3>
             <h5>Intern</h5>
         </div>
-        <div class="card-body d-flex justify-content-center card-container">
+        <div class="card-body d-flex justify-content-center">
             <div class="card" style="width: 11rem;">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${res.id}</li>
@@ -98,6 +59,46 @@ function internRender(res) {
             </div>
         </div>
     </div>
+    `
+};
+
+// TODO: Create a function to generate markdown for html page
+function generateMarkdown(data) {
+    // console.log('In GenerateMarkdown:', data);
+    let htmlVariable = '';
+    
+    for (var i = 0; i < data.length; i++) {
+        // console.log('Employees: ', data[i]);
+        if (data[i] === Manager) {
+            htmlVariable += managerRender(data[i]);
+        }
+        else if (data[i] === Engineer) {
+            htmlVariable += engineerRender(data[i]);
+        }
+        else if (data[i] === Intern) {
+            htmlVariable += internRender(data[i]);
+        }
+    }
+    return `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profile</title>
+    <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+    </head>
+    <body class="min-100-vh flex-column">
+    <main class="flex-row justify-center align-center col-auto p-4 bg-light">
+        <div class="card-container">
+            ${htmlVariable}
+        </div>
+    </main>
+    </body>
+</html>
     `
 };
   
